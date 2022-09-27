b9627252a23066e4ed4182590f36b19476913f3b
Matos-Jonathan <jonathan2018.matos@gmail.com>
2022-09-27 18:46:46 -0300

    criando um novo schame

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_214235) do
+  create_table "clientes", force: :cascade do |t|
+    t.string "nome"
+    t.string "cpf"
+    t.string "telefone"
+    t.string "email"
+    t.date "dataNascimento"
+    t.string "cidade"
+    t.string "bairro"
+    t.string "logradouro"
+    t.string "cep"
+    t.string "complemento"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "servicos", force: :cascade do |t|
+    t.string "nome"
+    t.string "descricao"
+    t.float "valor"
+    t.integer "trabalhador_id_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trabalhador_id_id"], name: "index_servicos_on_trabalhador_id_id"
+  end
+
+  create_table "trabalhadors", force: :cascade do |t|
+    t.string "nome"
+    t.string "cpf_or_cnpj"
+    t.string "telefone"
+    t.string "email"
+    t.date "dataNascimento"
+    t.string "profissao"
+    t.string "cidade"
+    t.string "bairro"
+    t.string "logradouro"
+    t.string "cep"
+    t.string "complemento"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "servicos", "trabalhador_ids"
 end
