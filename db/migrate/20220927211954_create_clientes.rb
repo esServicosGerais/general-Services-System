9afdf3551cb8468fdc195cab22f8e08ec17cea1d
Matos-Jonathan <jonathan2018.matos@gmail.com>
2022-09-27 18:20:10 -0300

    criando o scaffold de cliente

---
+++ db/migrate/20220927211954_create_clientes.rb
@@ -0,0 +1,18 @@
+class CreateClientes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clientes do |t|
+      t.string :nome
+      t.string :cpf
+      t.string :telefone
+      t.string :email
+      t.date :dataNascimento
+      t.string :cidade
+      t.string :bairro
+      t.string :logradouro
+      t.string :cep
+      t.string :complemento
+
+      t.timestamps
+    end
+  end
+end
