7e9c1c6d965d8fa58856ea13c3742997f2415600
Matos-Jonathan <jonathan2018.matos@gmail.com>
2022-09-27 22:00:35 -0300

    adicionando os validations relacionados aos atributos de endereço do cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -15,6 +15,18 @@ class Cliente < ApplicationRecord
 
   validates :dataNascimento, presence: true
 
+  #apenas letras, espassadas com um unico espaço, com tamanho minimo 4 é maximo de 30. Por possuir tamanho minomo,campo não pode ser vazio
+  validates :cidade, :format => {:with => /(^([A-Za-z\u00C0-\u017F]\s?){4,30}$)/,
+                                 :message => "Campo vazio, ou caracteres inválidos. Digite somente letras!"}
+
+  validates :logradouro, :format => {:with => /(^([A-Za-z0-9\u00C0-\u017F]|,||.|\s?){4,30}$)/,
+                                     :message => "Campo vazio, ou caracteres inválidos."}
+
+  validates :cep, format: {with: /(^[0-9]{5}-[0-9]{3}$)/, :messege => "digite no formato xxxxx-xxx"}
+
+  validates :complemento, :format => {:with => /(^([A-Za-z0-9\u00C0-\u017F]|,||.|\s?){,20}$)/,
+                                      :message => "Digite Caracteres válidos."}
+
   private
   def cpf_valida?
     if cpf.present? && !CPF.valid?(cpf)
