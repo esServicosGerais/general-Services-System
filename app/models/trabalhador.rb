258dfdae1346b57709b3970c785c4c0361d1845d
Matos-Jonathan <jonathan2018.matos@gmail.com>
2022-09-27 21:59:21 -0300

    adicionando os validations para atributos relacionado a endereço

--- app/models/trabalhador.rb
+++ app/models/trabalhador.rb
@@ -10,7 +10,7 @@ class Trabalhador < ApplicationRecord
   validates :cpf_or_cnpj, presence: true, uniqueness: true
   validate :valida_cpf_and_cnpj?
 
-  validates :telefone, :format => {with: /(^([0-9]{2}) [0-9]{5}-[0-9]{4}$)/,
+  validates :telefone, :format => {with: /(^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$)/,
                                    message: "Formato do número: (XX) XXXXX-XXXX"}
 
   validates :email, :format => {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create}, uniqueness: true
@@ -19,6 +19,17 @@ class Trabalhador < ApplicationRecord
 
   validates :profissao, :format => {with: /(^([A-Za-z\u00C0-\u017F]\s?){4,50}$)/,
                                     message: "Campo vazio, ou caracteres inválidos!"}
+  #apenas letras, espassadas com um unico espaço, com tamanho minimo 4 é maximo de 30. Por possuir tamanho minomo,campo não pode ser vazio
+  validates :cidade, :format => {:with => /(^([A-Za-z\u00C0-\u017F]\s?){4,30}$)/,
+                                 :message => "Campo vazio, ou caracteres inválidos. Digite somente letras!"}
+
+  validates :cep, format: {with: /(^[0-9]{5}-[0-9]{3}$)/, :messege => "digite no formato xxxxx-xxx"}
+
+  validates :logradouro, :format => {:with => /(^([A-Za-z0-9\u00C0-\u017F]|,||.|\s?){4,30}$)/,
+                                     :message => "Campo vazio, ou caracteres inválidos."}
+
+  validates :complemento, :format => {:with => /(^([A-Za-z0-9\u00C0-\u017F]|,||.|\s?){,20}$)/,
+                                      :message => "Digite Caracteres válidos."}
 
   private
   def valida_cpf_and_cnpj?
