d179fcdf7bde91ae9a34840d02641d065df1ca7e
Matos-Jonathan <jonathan2018.matos@gmail.com>
2022-10-09 14:34:46 -0300

    implementação do valitadion para dataInicio do model contrato

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -1,4 +1,13 @@
 class Contrato < ApplicationRecord
   belongs_to :cliente
   belongs_to :servico
+
+  validates :dataInicio, presence: true
+  validate :data_valida
+
+  def data_valida
+    if data.present? && data < Time.zone.today
+      errors.add(:data, " não pode ser uma data no passado")
+    end
+  end
 end
