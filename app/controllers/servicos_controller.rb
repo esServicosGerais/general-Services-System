da74b83b90b46ffd3e7fc6e711fab9396c473893
Breno <breno222.bm@gmail.com>
2022-10-10 01:10:22 -0300

    CONFIGURANDO ACESSO AS FUNCIONALIDADES DO SISTEMAS

--- app/controllers/servicos_controller.rb
+++ app/controllers/servicos_controller.rb
@@ -1,5 +1,5 @@
 class ServicosController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[ edit update destroy ] 
   before_action :set_servico, only: %i[ show edit update destroy ]
 
   # GET /servicos or /servicos.json
